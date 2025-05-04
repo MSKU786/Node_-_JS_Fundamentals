9467fd8fac5268795866f196276416216f7c559b
MSKU786 <manishsingh332000@gmail.com>
2025-05-04 12:33:36 +0530

    Adding differnt exmaple for map filter and reduce

--- Javascript/map_filter_reduce/sum_positive_element.js
+++ Javascript/map_filter_reduce/sum_positive_element.js
@@ -9,3 +9,11 @@ const ans = input.reduce((acc, curr) => {
 });
 
 console.log(input, ans);
+
+// Map is used when you want to return new array with updated values
+const mapAns = input.map((a) => a * 2);
+console.log(mapAns);
+
+// Filter is used to filer values it will also return a new array
+const inputFilter = input.filter((a) => a >= 0);
+console.log(inputFilter);
