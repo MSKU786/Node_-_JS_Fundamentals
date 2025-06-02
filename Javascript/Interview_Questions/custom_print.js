c25e519f87973f636374a9813024b9a4d88e958b
MSKU786 <manishsingh332000@gmail.com>
2025-06-02 08:16:25 +0530

    Custome print for obj

---
+++ Javascript/Interview_Questions/custom_print.js
@@ -0,0 +1,31 @@
+const obj = {
+  name: 'Manish',
+  phoneNumber: '+914422342',
+  address: {
+    flat: '2323',
+    zipcode: 222342,
+    houseNumber: '12/22',
+  },
+  subjects: {
+    branch: 'CSE',
+    core: {
+      computer_network: 23,
+      data_structure_algo: 40,
+    },
+  },
+};
+
+console.log(print('obj.subjects.core.data_structure_algo'));
+
+function print(path) {
+  let keys = path?.split('.');
+  //console.log(keys);
+  keys.shift();
+
+  let tempObj = obj;
+  for (let key of keys) {
+    tempObj = tempObj[key];
+  }
+
+  return tempObj;
+}
