85f70abf8c328221fc9b03fba592229e0c44819a
MSKU786 <manishsingh332000@gmail.com>
2025-07-11 11:02:34 +0530

    Removed synchronous resolve

--- Javascript/Interview_Questions/custom_promise_all_settled.js
+++ Javascript/Interview_Questions/custom_promise_all_settled.js
@@ -4,14 +4,18 @@ const customPromiseAllSettled = (tasks) => {
   }
   const promise = new Promise((resolve, reject) => {
     let solutions = new Array(tasks.length).fill(null);
+    let completed = 0;
     for (let i = 0; i < tasks.length; i++) {
-      tasks[i]
+      Promise.resolve(tasks[i])
         .then((res) => {
-          solutions[i] = res;
+          solutions[i] = { status: 'fulfilled', res };
         })
-        .catch((err) => (solutions[i] = err));
+        .catch((err) => (solutions[i] = { status: 'reject', err }))
+        .finally(() => {
+          completed++;
+          if (completed === tasks.length) resolve(solutions);
+        });
     }
-    resolve(solutions);
   });
 
   return promise;
