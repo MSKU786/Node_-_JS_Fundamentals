5b17e8b0ee9ef4d46e2fd19f880fd978dc0bda79
MSKU786 <manishsingh332000@gmail.com>
2025-07-14 08:54:05 +0530

    Added push and pop method with 2 hashmap approach

--- Node.js/Interview-Questions/Zupee/custom_stack.js
+++ Node.js/Interview-Questions/Zupee/custom_stack.js
@@ -12,13 +12,43 @@ If there is a tie for the most frequent element, the element closest to the stac
 class FreqStack {
   countMap;
   orderMap;
-
+  currentMax = 0;
   constructor() {
     this.countMap = new Map();
     this.orderMap = new Map();
   }
 
-  push(val) {}
+  push(val) {
+    if (this.countMap.has(val)) {
+      let count = this.countMap.get(val);
+      this.countMap.set(val, count + 1);
+      if (this.orderMap.has(count + 1)) {
+        let currentList = this.orderMap.get(count + 1);
+        currentList.push(val);
+      } else {
+        this.orderMap.set(count + 1, [val]);
+      }
+      this.currentMax = Math.max(this.currentMax, count + 1);
+    } else {
+      this.countMap.set(val, 1);
+      let currentList = this.orderMap.get(1);
+      currentList.push(val);
+      this.currentMax = Math.max(this.currentMax, 1);
+    }
+  }
 
-  pop() {}
+  pop() {
+    if (this.orderMap.has(this.currentMax)) {
+      let currentList = this.orderMap.get(this.currentMax);
+      let element = this.currentList.pop();
+      if (currentList.length === 0) {
+        this.orderMap.delete(this.currentMax);
+        this.currentMax--;
+      }
+      this.countMap.set(element, this.countMap.get(element) - 1);
+    } else {
+      this.currentMax--;
+      this.pop();
+    }
+  }
 }
