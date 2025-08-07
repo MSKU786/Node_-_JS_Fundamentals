3db039f1b1fe36d4f94fce94bd0a9b0f7126d1dd
MSKU786 <manishsingh332000@gmail.com>
2025-08-07 11:00:11 +0530

    ALternate level order traversal

--- Node.js/Interview-Questions/Wework/first_round.js
+++ Node.js/Interview-Questions/Wework/first_round.js
@@ -1,23 +1,18 @@
-// const list = [2,1,1,3,3,4,4,4,4];
+const list = [2,1,1,3,3,4,4,4,4];
 
-// //n2 => present [[2,1]], 2 -> 1
+//n2 => present [[2,1]], 2 -> 1
+const map = new Map();
+for (let i of list) {
+    if (map.has(i)) {
+        map.set(i, map.get(i)+1);
+    } else {
+        map.set(i, 1);
+    }
+}
 
-// const map = new Map();
-
-// for (let i of list) {
-//     if (map.has(i)) {
-//         map.set(i, map.get(i)+1);
-//     } else {
-//         map.set(i, 1);
-//     }
-
-
-
-// }
-
-// for (let key of map) {
-//     console.log(key);
-// } 
+for (let key of map) {
+    console.log(key);
+} 
 
 
 let shops = [4,1,2,5,6,2,3]
@@ -27,12 +22,10 @@ function pickToys(shops, index, lastIndex, dp) {
         return 0;
     }
     
-    //console.log(index);
     if (dp[index] != -1) {
         return dp[index]
     }
-        
-    
+         
     return dp[index] = Math.max(shops[index] + pickToys(shops, index+2, lastIndex, dp), pickToys(shops, index+1, lastIndex, dp));
 }
 
@@ -47,15 +40,42 @@ function main(shops) {
     console.log(Math.max(withFirst, withoutFirst));
 }
 
-let map = new Map();
-function(root) {
+
+let treeMap = new Map();
+
+class Node {
+    left = null;
+    right = null;
+    val;
+
+    constructor(val) {
+        this.val = val;
+    }
+}
+
+function alternateLevelOrderTraversal(root) {
     let queue = [];
+    let maxH = 0;
     queue.push([root,0]);
     while (!queue.isEmpty()) {
         let node = queue.shift();
+        maxH = Math.max(node[1], maxH)
         if (node.left != null)
             queue.push([node[0].left, node[1]+1);
         if (node.right != null) 
             queue.push(node[0].right, node[1]+1);
-        map,set(node[1], map.get(node[1]).add(node[0]))
+
+        if (map.has(node[1])) 
+            map.set(node[1], map.get(node[1]).add(node[0]))
+        else 
+            map.set(node[1], [node[0]])
     }
+
+    for (let i=0; i<maxH; i++) {
+        if (i%2 == 0)
+            console.log(map.get(i));
+        else 
+            console.log(map.get(i).reverse());
+    }
+}
+
