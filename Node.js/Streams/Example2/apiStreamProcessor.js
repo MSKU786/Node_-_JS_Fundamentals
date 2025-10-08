5c6fcf9857c1a96f8ceec856824fa65064cfbdd4
MSKU786 <manishsingh332000@gmail.com>
2025-10-08 11:33:15 +0530

    Added transformed data class

--- Node.js/Streams/Example2/apiStreamProcessor.js
+++ Node.js/Streams/Example2/apiStreamProcessor.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
+import { Readable } from 'stream';
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
@@ -10,18 +11,46 @@ const fetchDataAsync = async () => {
 };
 
 // Convert the json data to a readable stream
-
 const jsonArrayToStream = (data) => {
+  // Track which item we are currently processing
   let index = 0;
+
   const stream = new Readable({
+    // objectMode: false because we're pushing stringified JSON
     objectMode: true,
+
+    // read() is called whenever the consumer wants more data
     read() {
       if (index < data.length) {
-        stream.push(JSON.stringify(data[index++]));
+        this.push(JSON.stringify(data[index++]));
       } else {
-        stream.push(null);
+        this.push(null);
       }
     },
   });
   return stream;
 };
+
+class TransformStream extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  async _transform(chunk, encoding, callback) {
+    try {
+      const upperTitle = chunk.title.toUpperCase();
+      const joke = await fetch('https://api.chucknorris.io/jokes/random');
+      const jokeData = await joke.json();
+      const transformedChunk = {
+        id: chunk.id,
+        title: upperTitle,
+        joke: jokeData.value,
+      };
+
+      this.push(JSON.stringify(transformedChunk));
+      callback();
+    } catch (err) {
+      callback(err);
+    }
+  }
+}
