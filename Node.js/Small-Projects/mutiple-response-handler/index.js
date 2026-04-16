3e95a05e099e1ffb815d6a933945b7aec79f2747
MSKU786 <manishsingh332000@gmail.com>
2026-04-16 10:32:04 +0530

    Approach 2 or Approach 3 XML builder

--- Node.js/Small-Projects/mutiple-response-handler/index.js
+++ Node.js/Small-Projects/mutiple-response-handler/index.js
@@ -7,7 +7,6 @@ const app = express();
 const xmlBuilder = new Builder();
 
 // Middleware : Detect format form accept header
-
 app.use((req, res, next) => {
   const accept = req.header['accept'] || '';
   res.locals.format = accept.includes('application/xml') ? 'xml' : 'json';
@@ -34,6 +33,27 @@ function sendResponse(res, data) {
     res.json(data);
   }
 }
+
+// Approach 2: Query Parameter ?format=xml
+app.use((req, res, next) => {
+  res.locals.format = req.query.format === 'xml' ? 'xml' : 'json';
+  next();
+});
+
+app.get('/v2/users', (req, res) => {
+  const data = { users: { user: [{ id: 1, name: 'Alice' }] } };
+  sendResponse(res, data);
+});
+
+// Approach 3 - URL prefix /xml... vs /json/....
+app.get('/json/users', (req, res) => {
+  res.json(getUsers());
+});
+
+app.get('/xml/users', (req, res) => {
+  res.set('Content-Type', 'application/xml');
+  res.send(xmlBuilder.buildObject(getUsers()));
+});
 
 app.listen(4000, () => {
   console.log('Running on port 3000');
