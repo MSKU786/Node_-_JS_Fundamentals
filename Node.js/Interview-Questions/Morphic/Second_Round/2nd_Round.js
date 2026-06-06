3d970ed83ca75cf48746c0bb95d85bb19aaf5297
MSKU786 <manishsingh332000@gmail.com>
2026-06-06 15:14:44 +0530

    Clearing fundamentals of Promises

--- Node.js/Interview-Questions/Morphic/Second_Round/2nd_Round.js
+++ Node.js/Interview-Questions/Morphic/Second_Round/2nd_Round.js
@@ -37,3 +37,63 @@ function practice1() {
 
   console.log('5');
 }
+
+
+function practice2() {
+  console.log('1');
+
+  async function foo() {
+    console.log('2');
+    await Promise.resolve();
+    console.log('3');
+    await Promise.resolve();
+    console.log('4');
+  }
+
+  setTimeout(() => console.log('5'), 0);
+  foo()
+
+  Promise.resolve()
+    .then(() => console.log('6'))
+    .then(() => console.log('7'));
+  
+  console.log('8')
+
+  // 1 2 8 3 6 7 4 5 
+}
+
+
+/// Practive myPromiseALl
+
+
+const myPromiseAll = (tasks) => {
+  return new Promise((resolve,reject) => {
+    const n = tasks.length;
+    const results = new Array(n);
+    let completed = 0;
+    for (let i=0; i<n; i++) {
+      tasks[i].then((result) => {
+        results[i] = result;
+        completed++;
+        if (completed === n) {
+          resolve(results);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    }
+  })
+}
+
+const tasks = [
+  () => new Promise((resolve) => setTimeout(() => resolve('A'), 1000)),
+  () => Promise.resolve('B'),
+  () => new Promise((resolve) => setTimeout(() => resolve('C'), 3000)),
+  () => Promise.resolve('D'),
+]
+
+myPromiseAll(tasks)
+  .then((results) => console.log(results))
+  .catch((err) => console.log(err));
+
